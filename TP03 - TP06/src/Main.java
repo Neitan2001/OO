bdf2fb974075cfa65fb39b46358272b1b34e170f
isaac <isaac@DESKTOP-3IVMOUV>
2021-03-12 09:39:04 -0300

    Segundo teste

--- TP03 - TP06/src/Main.java
+++ TP03 - TP06/src/Main.java
@@ -4,6 +4,7 @@ public class Main {
 		public static void main(String[] args) {
 			System.out.print("Hello World em Java.");
 			System.out.print("  Cassete ein cara, tô programando em Java papai kkkkkkkk");
+			System.out.println("TEste");
 
 		}
 }
